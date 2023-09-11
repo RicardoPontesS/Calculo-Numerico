2c2cfa1b8d05fced4bf7185a1e12446aa02971d3
Ricardo <105815500+RicardoPontesS@users.noreply.github.com>
2023-09-10 22:22:41 -0300

    Update 2º.py

--- Ponto Fixo/1bb/2º.py
+++ Ponto Fixo/1bb/2º.py
@@ -100,6 +100,8 @@ def iteracoes(funcao_continua, intervalos):
     epsilon = 0.0000001
 
     for intervalo in intervalos:
+        if(intervalo[0]==0):
+            continue
         cont = 1
         x0 = (intervalo[0] + intervalo[1]) / 2  # Ponto médio do intervalo
         funcao = funcao_continua
@@ -179,8 +181,6 @@ if __name__ == '__main__':
     funcao_continua = verificaFuncoes(intervalos)
 
     if funcao_continua:
-        #if verificaDerivadaContinua(funcao_continua, intervalos):
-            #print("A derivada é contínua em todos os intervalos encontrados.")
         iteracoes(funcao_continua, intervalos)
     else:
         print("\nNenhuma função contínua encontrada.")
